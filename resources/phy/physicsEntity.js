efeb895caa0c77af3e34a51a326360760cac83e4
shupingl <254651804@qq.com>
2018-03-03 23:18:04 +0800

    引入物理引擎的一些基类

--- resources/phy/physicsEntity.js
+++ resources/phy/physicsEntity.js
@@ -105,27 +105,27 @@ PhysicsEntity.ELASTIC = 'elastic';
 //碰撞检测器
 // Rect collision tests the edges of each rect to
 // test whether the objects are overlapping the other
-CollisionDetector.prototype.collideRect = 
-    function(collider, collidee) {
+// CollisionDetector.prototype.collideRect = 
+//     function(collider, collidee) {
  
-    // Store the collider and collidee edges
-    var l1 = collider.getLeft();
-    var t1 = collider.getTop();
-    var r1 = collider.getRight();
-    var b1 = collider.getBottom();
+//     // Store the collider and collidee edges
+//     var l1 = collider.getLeft();
+//     var t1 = collider.getTop();
+//     var r1 = collider.getRight();
+//     var b1 = collider.getBottom();
  
-    var l2 = collidee.getLeft();
-    var t2 = collidee.getTop();
-    var r2 = collidee.getRight();
-    var b2 = collidee.getBottom();
+//     var l2 = collidee.getLeft();
+//     var t2 = collidee.getTop();
+//     var r2 = collidee.getRight();
+//     var b2 = collidee.getBottom();
  
-    // If the any of the edges are beyond any of the
-    // others, then we know that the box cannot be
-    // colliding
-    if (b1 < t2 || t1 > b2 || r1 < l2 || l1 > r2) {
-        return false;
-    }
+//     // If the any of the edges are beyond any of the
+//     // others, then we know that the box cannot be
+//     // colliding
+//     if (b1 < t2 || t1 > b2 || r1 < l2 || l1 > r2) {
+//         return false;
+//     }
  
-    // If the algorithm made it here, it had to collide
-    return true;
-};
+//     // If the algorithm made it here, it had to collide
+//     return true;
+// };
