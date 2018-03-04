940feba9a14fad328db411bb156e5879bd6dc1de
shupingl <254651804@qq.com>
2018-03-04 21:14:58 +0800

    重构代码,使其更为合理

--- resources/game/enter.js
+++ resources/game/enter.js
@@ -2,19 +2,26 @@
 var addSprites = function () {
 
     //需要渲染的小精灵列表
-    var sprites = window.Gamex.sprites;
+    var sprites = [];
     //渲染列表
     sprites.push(new bg(4));
     sprites.push(new bg2(4));
     sprites.push(new frameLabel(3));
     sprites.push(new startButton(2));
 
-    sprites.push(new car("player1", "elastic", "dynamic", true, 99, 2, 180, 380, 0, 0, 0, 0, 0));
-    sprites.push(new car("player2", "elastic", "dynamic", true, 100, 1, 580, 380, -100, 0, 0, 0, 0));
+    sprites.push(new car("player1", "elastic", "dynamic", true, 100, 2, 180, 380, 0, 0, 0, 0, 0));
+    sprites.push(new car("player2", "elastic", "dynamic", true, 99, 1, 580, 380, -100, 0, 0, 0, 0));
 
     //sprites.push(new sprite2(cavs, 1));
     //sprites.push(new sprite3(cavs, 1));
-    sprites = _.orderBy(sprites, ['deep'], ['desc']);
+    //对sprites进行排序
+    sprites = _.orderBy(sprites, ['deep'], ['asc']);
+
+    //并将其push进精灵渲染数组
+    for (let i = 0; i < sprites.length; i++) {
+        window.Gamex.sprites.push(sprites[i]);
+    }
+
 
 }
 
